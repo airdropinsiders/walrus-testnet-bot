fc8f145d03d9207e2f80f6af47d1c01f411da7ec
INSIDERS <31087501+airdropinsiders@users.noreply.github.com>
2024-10-25 13:45:34 +0700

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,86 +1,82 @@
 import { privateKey } from './accounts/accounts.js';
 import { Config } from './config/config.js';
 import { COINENUM } from './src/core/coin/coin_enum.js';
-import a0_0x52d6f4 from './src/core/core.js';
+import a0_0x3a450d from './src/core/core.js';
 import { Helper } from './src/utils/helper.js';
-import a0_0x47fbca from './src/utils/logger.js';
-async function operation(_0xcba1ac) {
-  const _0x47337e = new a0_0x52d6f4(_0xcba1ac);
+import a0_0x140d1b from './src/utils/logger.js';
+async function operation(_0x320205) {
+  const _0xffaa87 = new a0_0x3a450d(_0x320205);
   try {
-    await _0x47337e.getAccountInfo();
-    await _0x47337e.getBalance(true);
-    await _0x47337e.requestFaucet();
-    const _0x3fb804 = await _0x47337e.checkNFT();
-    if (_0x3fb804 == false) {
-      await _0x47337e.mintNft();
+    await _0xffaa87.getAccountInfo();
+    await _0xffaa87.getBalance(true);
+    await _0xffaa87.requestFaucet();
+    const _0x1bc429 = await _0xffaa87.checkNFT();
+    if (_0x1bc429 == false) {
+      await _0xffaa87.mintNft();
     }
-    const _0x3d794e = _0x47337e.balance;
-    const _0x67e942 = _0x3d794e.find(_0x3b1ae4 => _0x3b1ae4.coinType === COINENUM.SUI);
-    const _0x36ce85 = _0x67e942 ? _0x67e942.totalBalance : 0x0;
-    for (const _0x273ac3 of new Array(Config.SWAPCOUNT)) {
-      if (_0x36ce85 < Config.TXAMOUNTMAX) {
+    const _0x13d7cf = _0xffaa87.balance;
+    const _0x47c2bd = _0x13d7cf.find(_0x560197 => _0x560197.coinType === COINENUM.SUI);
+    const _0x486388 = _0x47c2bd ? _0x47c2bd.totalBalance : 0x0;
+    for (const _0xf7806c of new Array(Config.SWAPCOUNT)) {
+      if (_0x486388 < Config.TXAMOUNTMAX) {
         throw Error("Min balance is " + Config.TXAMOUNTMAX + " SUI");
       }
       try {
-        await _0x47337e.exSuiToWal();
-        await _0x47337e.exWalToSui();
-        _0x47337e.txCount += 0x1;
-      } catch (_0x5c7037) {}
+        await _0xffaa87.exSuiToWal();
+        await _0xffaa87.exWalToSui();
+        _0xffaa87.txCount += 0x1;
+      } catch (_0x583576) {}
     }
-    if (_0x36ce85 < Config.TXAMOUNTMAX) {
+    if (_0x486388 < Config.TXAMOUNTMAX) {
       throw Error("Min balance is " + Config.TXAMOUNTMAX + " SUI");
     }
-    await _0x47337e.exSuiToWal();
-    await _0x47337e.stakeWalToOperator();
-    const _0x4c9b98 = Config.DELAYINHOURS ?? 0x6;
-    const _0x3f5dc0 = 3600000 * _0x4c9b98;
-    await Helper.delay(_0x3f5dc0, _0xcba1ac, "Account " + (privateKey.indexOf(_0xcba1ac) + 0x1) + " Processing Done, Delaying for " + Helper.msToTime(_0x3f5dc0), _0x47337e);
-    await operation(_0xcba1ac);
-  } catch (_0x52e31c) {
-    if (_0x52e31c.message) {
-      await Helper.delay(0x2710, _0xcba1ac, "Error : " + _0x52e31c.message + ", Retry again after 10 Second", _0x47337e);
+    await _0xffaa87.exSuiToWal();
+    await _0xffaa87.stakeWalToOperator();
+    const _0x480c33 = Config.DELAYINHOURS ?? 0x6;
+    const _0x5b6d88 = 3600000 * _0x480c33;
+    await Helper.delay(_0x5b6d88, _0x320205, "Account " + (privateKey.indexOf(_0x320205) + 0x1) + " Processing Done, Delaying for " + Helper.msToTime(_0x5b6d88), _0xffaa87);
+    await operation(_0x320205);
+  } catch (_0x38d82a) {
+    if (_0x38d82a.message) {
+      await Helper.delay(0x2710, _0x320205, "Error : " + _0x38d82a.message + ", Retry again after 10 Second", _0xffaa87);
     } else {
-      await Helper.delay(0x2710, _0xcba1ac, "Error :" + JSON.stringify(_0x52e31c) + ", Retry again after 10 Second", _0x47337e);
+      await Helper.delay(0x2710, _0x320205, "Error :" + JSON.stringify(_0x38d82a) + ", Retry again after 10 Second", _0xffaa87);
     }
-    await operation(_0xcba1ac);
+    await operation(_0x320205);
   }
 }
 async function startBot() {
-  const _0x15dd32 = {
-    'xLtaK': "Please input your account first on accounts.js file",
-    'ykiBG': function (_0x2883bb) {
-      return _0x2883bb();
-    }
+  const _0x1607bc = {
+    'lMmgE': function (_0x39ee12, _0x357c7e) {
+      return _0x39ee12(_0x357c7e);
+    },
+    'iEKVy': "kdaAi"
   };
-  return new Promise(async (_0x221f31, _0x341731) => {
+  return new Promise(async (_0x250f5a, _0x320a33) => {
     try {
-      a0_0x47fbca.info("BOT STARTED");
+      a0_0x140d1b.info("BOT STARTED");
       if (privateKey.length == 0x0) {
         throw Error("Please input your account first on accounts.js file");
       }
-      const _0x1535c5 = [];
-      for (const _0x69befc of privateKey) {
-        _0x1535c5.push(operation(_0x69befc));
+      const _0x2f9b84 = [];
+      for (const _0x48f4fb of privateKey) {
+        _0x2f9b84.push(operation(_0x48f4fb));
       }
-      await Promise.all(_0x1535c5);
-      _0x221f31();
-    } catch (_0x34d74d) {
-      a0_0x47fbca.info("BOT STOPPED");
-      a0_0x47fbca.error(JSON.stringify(_0x34d74d));
-      _0x341731(_0x34d74d);
+      await Promise.all(_0x2f9b84);
+      _0x250f5a();
+    } catch (_0x437332) {
+      a0_0x140d1b.info("BOT STOPPED");
+      a0_0x140d1b.error(JSON.stringify(_0x437332));
+      _0x320a33(_0x437332);
     }
   });
 }
 (async () => {
-  const _0x17ba49 = {
-    'wYvik': 'jGKWC',
-    'CEpqr': "lhkwr"
-  };
   try {
-    a0_0x47fbca.clear();
-    a0_0x47fbca.info('');
-    a0_0x47fbca.info("Application Started");
-    console.log("WALRUS BOT");
+    a0_0x140d1b.clear();
+    a0_0x140d1b.info('');
+    a0_0x140d1b.info("Application Started");
+    console.log("SUI WALRUS BOT");
     console.log();
     console.log("Join Channel : https://t.me/AirdropInsiderID");
     console.log("Dont forget to run git pull to keep up to date");
@@ -91,8 +87,8 @@ async function startBot() {
       throw Error("Please set up accounts.js first");
     }
     await startBot();
-  } catch (_0x178215) {
-    console.log("Error During executing bot", _0x178215);
-    throw _0x178215;
+  } catch (_0x2c2e71) {
+    console.log("Error During executing bot", _0x2c2e71);
+    throw _0x2c2e71;
   }
 })();
